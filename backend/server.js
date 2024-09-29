659adabdbe00cbbaf114601186b9a1d007a1702c
UjjawalGusain <ujjawalgusain31@gmail.com>
2024-09-29 14:26:28 +0530

    Testing deployment

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const connectDB = require('./config/db')
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: '*', 
     methods: 'GET,POST,PUT,DELETE',  
     credentials: true             
   }));
